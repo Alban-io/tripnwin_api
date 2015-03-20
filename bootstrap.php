b59e96f299392c7c3c124bc86735db2e5404ab2e
Nicolas Charlot <nicolas.charlot@isics.fr>
2015-03-20 18:26:10 +0100

    Added service DoctrineServiceProvider

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,15 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = $debug;
 
+// Services
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'host'     => $mysql_host,
+        'dbname'   => $mysql_database,
+        'user'     => $mysql_user,
+        'password' => $mysql_password,
+    ),
+));
+
 return $app;
